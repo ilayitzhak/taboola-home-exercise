9a5079dd817ded0850e9ee1350863226cc5b3319
ilayitzhak <ilayy1324@gmail.com>
2024-10-01 10:09:08 +0300

    Added UrlItem.css and UrlItem.js

--- src/components/UrlItem/UrlItem.js
+++ src/components/UrlItem/UrlItem.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import './UrlItem.css'; // Importing the CSS file for custom styling
 
 const UrlItem = ({ enrichedUrl }) => {
     const { retrievedUrl, enrichmentData } = enrichedUrl;
 
     return (
-        <div key={retrievedUrl.href}>
-            <h2>{retrievedUrl.href}</h2>
-            <p>ID: {enrichmentData._id}</p>
-            <p>URL: {enrichmentData.url}</p>
-            <p>Name: {enrichmentData.name}</p>
-            <p>Estimated Employees: {enrichmentData.est_emp}</p>
-            <p>Industry: {enrichmentData.industry}</p>
-            <p>Annual Revenue: {enrichmentData.annual_rev}</p>
-            <p>Country: {enrichmentData.country}</p>
+        <div className="card">
+            <div className="card-content">
+                <h1>{enrichmentData.name}</h1>
+                
+                <p><strong>ID:</strong> {enrichmentData._id}</p>
+                <p><strong>URL: </strong> 
+                    <a href={enrichmentData.url} target="_blank" rel="noopener noreferrer">
+                        {enrichmentData.url}
+                    </a>
+                </p>
+
+                <p><strong>Name:</strong> {enrichmentData.name}</p>
+                <p><strong>Estimated Employees:</strong> {enrichmentData.est_emp}</p>
+                <p><strong>Industry:</strong> {enrichmentData.industry}</p>
+                <p><strong>Annual Revenue:</strong> {enrichmentData.annual_rev}</p>
+                <p><strong>Country:</strong> {enrichmentData.country}</p>
+                <h2 className="url-title small-text"><strong><u>Retrieve URL:</u></strong> {retrievedUrl.href}</h2>
+            </div>
         </div>
     );
 };
